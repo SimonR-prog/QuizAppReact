b097983cdf6133f536ebd0eca553f23ee4a21b42
Simon <152021717+SimonR-prog@users.noreply.github.com>
2025-01-17 22:56:59 +0100

    Created a structure to try.

--- assignmentFE2/src/App.jsx
+++ assignmentFE2/src/App.jsx
@@ -1,28 +1,12 @@
 import './App.css'
 
-
 function App() {
-  const correctAnswer = 0;
-  const wrongAnswer = 0;
   return (
     <>
-      <h1>Quiz game!</h1>
-      <div className="card">
-        <button >Start</button>
-        <p>
-          Correct Answers: {correctAnswer}
-        </p>
-        <p>
-          Wrong Answers: {wrongAnswer}
-        </p>
-        <div>
-          <button id="option-one" className="option"></button>
-          <button id="option-two" className="option"></button>
-          <button id="option-three" className="option"></button>
-        </div>
+      <div className="start-card">
+        <button id="start" className="start-btn btn">Click to start the quiz</button>
       </div>
     </>
   )
 }
-
 export default App
