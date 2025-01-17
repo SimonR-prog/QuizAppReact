9eeeb14669368801fb37aabd4e081929f59985c2
Simon <152021717+SimonR-prog@users.noreply.github.com>
2025-01-17 23:03:01 +0100

    Created footer. Added links.

---
+++ assignmentFE2/src/footer.jsx
@@ -0,0 +1,15 @@
+
+function Footer(){
+    return (
+    <>
+        <footer className="footer">
+            <ul className="footer-list">
+                <li><p>© Simon Rollén</p></li>
+                <li><a href="https://www.linkedin.com/in/simon-roll%C3%A9n-250a5232a/">LinkedIn</a></li>
+                <li><a href="https://github.com/SimonR-prog">GitHub</a></li>
+            </ul>
+        </footer>
+    </>
+    )
+}
+export default Footer
