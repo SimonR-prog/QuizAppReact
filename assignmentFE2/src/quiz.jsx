69c7c73fc0ac3259557848dae83d88f178b23a45
Simon <152021717+SimonR-prog@users.noreply.github.com>
2025-01-27 00:45:56 +0100

    Changes:

--- assignmentFE2/src/quiz.jsx
+++ assignmentFE2/src/quiz.jsx
@@ -1,47 +1,80 @@
 import GetData from './questions';
-import './quiz.css'
-import RnGenerator from "./Rng";
-import { useState } from "react";
-
-//Need a function that will compare the correct answer with the answer of the users click.
-//If answer === click -> increase wins etc. Can return a true/false and use an if return true ++ on wins if false ++ on loss..
-
-// function CorrectAnswer(){
-//     const correctAnswer = ;
-
-//     if (correctAnswer === ){
-//         setCorrect
-//     }
-// }
+import './quiz.css';
+import { useState, useEffect } from "react";
+import ResultScreen from "./result";
 
 function QuizGame(){
-    const arrayOfIds = RnGenerator();
+    const [data, setData] = useState([]);
+    const [showResultScreen, setShowResultScreen] = useState(false);
+    const [correct, setCorrect] = useState(0);
+    const [wrong, setWrong] = useState(0);
+    const [indexId, setIndexId] = useState(0);
+    const question = data[indexId]
+    const [color, setColor] = useState("")
 
-    const data = GetData();
-    console.log(data);
 
-    var [correct, setCorrect] = useState(0);
-    var [wrong, setWrong] = useState(0);
+    useEffect(() => {
+        const getData = async () => {
+            const data = await GetData();
+            setData(data);
+        };
+        getData();
+    }, []);
+    useEffect(() => {
+        document.body.style.backgroundColor = color
+    }, [color])
+
+    if (data.length === 0) {
+        return <p>Fetching questions...</p>;
+      }
+
+    function checkAnswer(chosenAnswer) {
+        if (chosenAnswer === question.answer) {
+            setCorrect(correct + 1);
+            setColor("green");
+        } else{
+            setWrong(wrong + 1);
+            setColor("red");
+        }
+        if (indexId + 1 < data.length) {
+            setIndexId(indexId + 1)
+        }
+        else {
+            setShowResultScreen(true);
+        }
+    };
 
     return(
         <>
-            <div className="quiz-card">
-                <h2>Question</h2>
-                <section className="result">
-                    <ul className="result-list">
-                        <li><p>Correct answers: {correct} </p></li>
-                        <li><p>Wrong answers: {wrong} </p></li>
-                    </ul>
-                </section>
-                <section className="answers">
-                    <ul className="answer-list">
-                        <li><button id="option-one" className="btn">Placeholder</button></li>
-                        <li><button id="option-two" className="btn">Placeholder</button></li>
-                        <li><button id="option-three" className="btn">Placeholder</button></li>
-                    </ul>
-                </section>
-            </div>
+            {showResultScreen ? (
+                <>
+                    <ResultScreen correct={correct} wrong={wrong} />
+                </>
+            ) : (
+                <>
+                    <section className="quiz-card">
+                        <p>{ question.question }</p>
+                        <section className="results">
+                            <h2>Score:</h2>
+                            <ul>
+                                <li><p>Correct: {correct}</p></li>
+                                <li><p>Wrong: {wrong}</p></li>
+                            </ul>
+                        </section>
+                        <section className="answers">
+                        <ul className="answer-list">
+                            {question.options.map((option, indexId) => (
+                                <li key={indexId}>
+                                    <button className="btn" onClick={() => checkAnswer(option)}>{option}
+                                    </button>
+                                </li>
+                                ))}
+                            </ul>
+                        </section>
+                    </section>
+                </>
+            )}
         </>
-    )
+    );
 }
 export default QuizGame
